4846712dcf2de8f104af6ebfa65eafe939ff66fe
flaxaaa <ilovebigbooty12345@gmail.com>
2026-05-30 17:24:01 -0400

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,5 +1,33 @@
 /* ==========================================================================
-   GAMES & APPLICATION DATA REGISTRY REGION (STANDBY)
+   GAMES & APPLICATION DATA REGISTRY REGION
    ========================================================================== */
-const gamesRegistry = [];
-const appsRegistry = [];
+
+const gamesRegistry = [
+  {
+    id: "basket.bros-game",
+    name: "Basket Bros",
+    image: "https://kevin.games/assets/images/new/basketbros-io.jpg", //
+    url: "basketbros.io"
+  }
+];
+
+const appsRegistry = [
+  {
+    id: "youtube-app",
+    name: "YouTube",
+    image: "https://www.iconsdb.com/icons/preview/color/FF0303/youtube-xxl.png", // 
+    url: "https://www.youtube.com/embed/"
+  },
+  {
+    id: "tiktok-app",
+    name: "TikTok",
+    image: "https://www.iconsdb.com/icons/preview/black/tiktok-xxl.png", // 
+    url: "about:blank"
+  },
+  {
+    id: "x-app",
+    name: "X",
+    image: "https://images.unsplash.com/photo-1690138871287-02b2fc3b87c2?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", // 
+    url: "x.com/home"
+  }
+];
